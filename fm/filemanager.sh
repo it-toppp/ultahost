878d9a7de87dfaffbefc58f7436b44530931775d
it-toppp <36742579+it-toppp@users.noreply.github.com>
2021-01-17 18:26:54 +0300

    Update filemanager.sh

--- fm/filemanager.sh
+++ fm/filemanager.sh
@@ -5,13 +5,11 @@ wget https://raw.githubusercontent.com/prasathmani/tinyfilemanager/master/tinyfi
 wget https://raw.githubusercontent.com/prasathmani/tinyfilemanager/master/translation.json
 wget https://raw.githubusercontent.com/prasathmani/tinyfilemanager/master/config.php
 wget https://raw.githubusercontent.com/it-toppp/ultahost/main/fm/index.php
-
 #nginx
 sed -i 's|client_max_body_size            256m|client_max_body_size  5120m|' /usr/local/hestia/nginx/conf/nginx.conf
 #php
-echo php_admin_value[post_max_size] = 5260M >> /usr/local/hestia/php/etc/php-fpm.conf
-echo php_admin_value[post_max_size] = 5260M >> /usr/local/hestia/php/etc/php-fpm.conf
+sed -i 's|\[post_max_size\] = 256M|\[post_max_size\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
+sed -i 's|\[upload_max_filesize\] = 256M|\[post_max_size\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
 systemctl restart hestia
-
 #conf
 sed -i.bak -e "s/\$root\_path = \$\_SERVER\['DOCUMENT_ROOT'\];/\$root_path = \'\/home\/admin\';/g"    /usr/local/hestia/web/fm/config.php
