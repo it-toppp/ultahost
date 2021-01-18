ac545dbb5acceeffd3ea5db4abc5ac34c6364651
it-toppp <36742579+it-toppp@users.noreply.github.com>
2021-01-18 09:59:16 +0300

    Update filemanager.sh

--- fm/filemanager.sh
+++ fm/filemanager.sh
@@ -13,15 +13,14 @@ wget https://raw.githubusercontent.com/prasathmani/tinyfilemanager/master/transl
 fi
 #nginx
 sed -i 's|client_max_body_size            256m|client_max_body_size  5120m|' /usr/local/hestia/nginx/conf/nginx.conf
-#sed -i 's|send_timeout                    60;|send_timeout  1200;|' /etc/nginx/nginx.conf
-sed -i 's|proxy_send_timeout              180|proxy_send_timeout  1200|' /etc/nginx/nginx.conf
-sed -i 's|proxy_read_timeout              300|proxy_read_timeout  1200|' /etc/nginx/nginx.conf
-
-#php
+#sed -i 's|send_timeout                    60;|send_timeout  1200;|' /usr/local/hestia/nginx/conf/nginx.conf
+sed -i 's|proxy_send_timeout              180|proxy_send_timeout  1200|' /usr/local/hestia/nginx/conf/nginx.conf
+sed -i 's|proxy_read_timeout              300|proxy_read_timeout  1200|' /usr/local/hestia/nginx/conf/nginx.conf
+ #php
 grep -rl  "_time] = 300" /usr/local/hestia/php/etc/ | xargs perl -p -i -e 's/_time] = 300/_time] = 1200/g'
 sed -i 's|\[post_max_size\] = 256M|\[post_max_size\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
 sed -i 's|\[upload_max_filesize\] = 256M|\[upload_max_filesize\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
 systemctl restart hestia
 #conf
 sed -i.bak -e "s/\$root\_path = \$\_SERVER\['DOCUMENT_ROOT'\];/\$root_path = \'\/home\/admin\';/g"    /usr/local/hestia/web/fm/config.php
-sed -i 's|max_upload_size_bytes = 2048|max_upload_size_bytes = 2048000000|' /usr/local/hestia/web/fm/config.php
+sed -i 's|max_upload_size_bytes = 2048|max_upload_size_bytes = 10000000000|' /usr/local/hestia/web/fm/config.php
