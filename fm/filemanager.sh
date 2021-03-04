2e01c685dbb4bedb8191c573965a462fabc944b3
it-toppp <36742579+it-toppp@users.noreply.github.com>
2021-03-04 22:31:37 +0300

    Update filemanager.sh

--- fm/filemanager.sh
+++ fm/filemanager.sh
@@ -15,6 +15,14 @@ wget https://raw.githubusercontent.com/prasathmani/tinyfilemanager/master/transl
 chmod 644 translation.json
 fi
 
+#conf
+sed -i.bak -e "s/\$root\_path = \$\_SERVER\['DOCUMENT_ROOT'\];/\$root_path = \'\/home\/admin\/web\';/g" config.php
+sed -i 's|max_upload_size_bytes = 2048|max_upload_size_bytes = 10000000000|' config.php
+sed -i 's|"show_hidden":false|"show_hidden":true|' tinyfilemanager.php
+sed -i "s|.*navbar-brand.*|        <a class="navbar-brand" href=\"/\"> Exit </a>|" tinyfilemanager.php
+sed -i 's|use_auth = true|use_auth = false|' config.php
+#sed -i "s|theme = 'light'|theme = \'dark\'|" config.php
+
 #nginx
 sed -i 's|client_max_body_size            256m|client_max_body_size  5120m|' /usr/local/hestia/nginx/conf/nginx.conf
 sed -i 's|proxy_send_timeout              180|proxy_send_timeout  1200|' /usr/local/hestia/nginx/conf/nginx.conf
@@ -25,13 +33,6 @@ grep -rl  "_time] = 300" /usr/local/hestia/php/etc/ | xargs perl -p -i -e 's/_ti
 sed -i 's|\[post_max_size\] = 256M|\[post_max_size\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
 sed -i 's|\[upload_max_filesize\] = 256M|\[upload_max_filesize\] = 5120M|' /usr/local/hestia/php/etc/php-fpm.conf
 systemctl restart hestia
-
-#conf
-sed -i.bak -e "s/\$root\_path = \$\_SERVER\['DOCUMENT_ROOT'\];/\$root_path = \'\/home\/admin\/web\';/g"    /usr/local/hestia/web/fm/config.php
-sed -i 's|max_upload_size_bytes = 2048|max_upload_size_bytes = 10000000000|' /usr/local/hestia/web/fm/config.php
-sed -i 's|"show_hidden":false|"show_hidden":true|' /usr/local/hestia/web/fm/tinyfilemanager.php
-sed -i 's|use_auth = true|use_auth = false|' /usr/local/hestia/web/fm/config.php
-#sed -i "s|theme = 'light'|theme = \'dark\'|" /usr/local/hestia/web/fm/config.php
 
 #chown root:admin config.php tinyfilemanager.php
 #chmod 660 config.php tinyfilemanager.php
