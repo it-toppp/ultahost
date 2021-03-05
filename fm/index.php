4f39b4da6468580bb870bb064e6413010e0af8c8
it-toppp <36742579+it-toppp@users.noreply.github.com>
2021-03-05 14:51:34 +0300

    Update index.php

--- fm/index.php
+++ fm/index.php
@@ -5,20 +5,6 @@ $TAB = 'USER';
 // Main include
 include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
 
-// Header
-//include($_SERVER['DOCUMENT_ROOT'].'/templates/1.html');
-$wstring = <<<MSG
-<html>
- <head>
-  <meta charset="utf-8">
-  <title>Hestia</title>
- </head>
- <body>
-  <button onclick="document.location='/'">Exit to Control Panel »</button>
- </body>
-</html>
-MSG;
-echo $wstring;
 // Data
 define('FM_EMBED', true);
 define('FM_SELF_URL', $_SERVER['PHP_SELF']);
