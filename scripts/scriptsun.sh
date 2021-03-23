37df9d6e66fcbeef4b83f13e585b649a82ec0c71
it-toppp <36742579+it-toppp@users.noreply.github.com>
2021-03-23 18:09:15 +0300

    Update scriptsun.sh

--- scripts/scriptsun.sh
+++ scripts/scriptsun.sh
@@ -2,41 +2,73 @@
 DOMAIN=$1
 SCRIPT=$2
 PURSHCODE=$3
-
+user=admin
+WORKINGDIR="/home/$user/web/$DOMAIN/public_html"
 #PASSWD=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\%\^\&\(\)-+= < /dev/urandom | head -c 12)
 DBPASSWD=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12)
 DB=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 5)
+DBNAME=$user_$DB
+DBUSER=$DBNAME
 IP=$(wget -O - -q ifconfig.me)
+email=admin@$DOMAIN
 
-if [ ! -d "/home/admin/web/$DOMAIN/public_html" ]; then
+echo "$IP  $DOMAIN" >> /etc/hosts
+v-add-database admin $DB $DB $DBPASSWD
+cd $WORKINGDIR
+rm -fr $WORKINGDIR/{*,.*} &> /dev/null
+
+if [ ! -d "/home/$user/web/$DOMAIN/public_html" ]; then
 v-add-web-domain admin $DOMAIN $IP yes www.$DOMAIN
 v-add-letsencrypt-domain admin $DOMAIN www.$DOMAIN
 fi
+
+if [ ! -f "/home/$user/conf/web/$DOMAIN/ssl/$DOMAIN.pem" ]; then
+    v-add-letsencrypt-domain "$user" "$DOMAIN" "www.$DOMAIN"
+fi
+
 if [ "$SCRIPT" = "wowonder-null" ] || [ "$SCRIPT" = "wowonder" ]; then
 v-change-web-domain-backend-tpl admin $DOMAIN PHP-7_2
 fi
 
-echo "$IP  $DOMAIN" >> /etc/hosts
-v-add-database admin $DB $DB $DBPASSWD
+function wordpress() {
+rm -rf /home/$user/wp
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /home/$user/wp
+cd /home/$user/web/$DOMAIN/public_html
+/home/$user/wp core download --allow-root
+/home/$user/wp core config --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASSWD --allow-root
+/home/$user/wp core install --url="$DOMAIN" --title="$DOMAIN" --admin_user="$admin" --admin_password="$DBPASSWD" --admin_email="$email" --path=$WORKINGDIR --allow-root
+#FIX za https://github.com/wp-cli/wp-cli/issues/2632
+mysql -u$DBUSER -p$DBPASSWD $DBNAME -e "update wp_options set option_value = 'https://$DOMAIN' where option_name = 'siteurl'; update wp_options set option_value = 'https://$DOMAIN' where option_name = 'home';"
+chown -R $user:$user $WORKINGDIR
+rm -rf /home/$user/wp
 
-cd /home/admin/web/$DOMAIN/public_html
-rm -fr /home/admin/web/$DOMAIN/public_html/{*,.*} &> /dev/null
+cat >> .htaccess <<EOF
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase ${URL_PATH}
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . ${URL_PATH}index.php [L]
+</IfModule>
+EOF
+
+}
+
+function scriptsun() {
 wget http://ss.ultahost.com/$SCRIPT.zip
 unzip -qo $SCRIPT.zip
-chmod 777 ffmpeg/ffmpeg upload cache ffmpeg/ffmpeg sys/ffmpeg/ffmpeg ./assets/import/ffmpeg/ffmpeg  &> /dev/null
 chown -R admin:admin ./
-
- if [ "$SCRIPT" = "pixelphoto" ]; then
-  mv ./install/index.php ./install.php_old
-  wget https://raw.githubusercontent.com/it-toppp/ultahost/main/scripts/pixelphoto/installer.php -O ./install/index.php
- fi
-
+chmod 777 ffmpeg/ffmpeg upload cache ffmpeg/ffmpeg sys/ffmpeg/ffmpeg ./assets/import/ffmpeg/ffmpeg  &> /dev/null
 curl -L --fail --silent --show-error --post301 --insecur \
      --data-urlencode "purshase_code=$PURSHCODE" \
      --data-urlencode "sql_host=localhost" \
-     --data-urlencode "sql_user=admin_$DB" \
+     --data-urlencode "sql_user=$DBUSER" \
      --data-urlencode "sql_pass=$DBPASSWD" \
-     --data-urlencode "sql_name=admin_$DB" \
+     --data-urlencode "sql_name=$DBNAME" \
      --data-urlencode "site_url=https://$DOMAIN" \
      --data-urlencode "siteName=$DOMAIN" --data-urlencode "site_name=$DOMAIN" \
      --data-urlencode "siteTitle=$DOMAIN" --data-urlencode "site_title=$DOMAIN" \
@@ -45,19 +77,37 @@ curl -L --fail --silent --show-error --post301 --insecur \
      --data-urlencode "admin_password=$DBPASSWD" \
      --data-urlencode "install=install" \
 http://$DOMAIN/install/?page=installation | grep -o -e "Failed to connect to MySQL" -e "successfully installed" -e "Wrong purchase code" -e "This code is already used on another domain"
-
-#wowonder,playtube,deepsound
-     mysql admin_$DB -e "UPDATE config SET value = 'on' WHERE  name = 'ffmpeg_system';" &> /dev/null
-     mysql admin_$DB -e "UPDATE config SET value = '/usr/bin/ffmpeg' WHERE  name = 'ffmpeg_binary_file';" &> /dev/null
+}
 
 #pixelphoto
-     mysql admin_$DB -e "UPDATE pxp_config SET value = 'on' WHERE  name = 'ffmpeg_sys';" &> /dev/null
-     mysql admin_$DB -e "UPDATE pxp_config SET value = '/usr/bin/ffmpeg' WHERE  name = 'ffmpeg_binary_file';" &> /dev/null
+if [ "$SCRIPT" = "pixelphoto" ]; then
+scriptsun
+  rm -f ./install/index.php
+  wget https://raw.githubusercontent.com/it-toppp/ultahost/main/scripts/pixelphoto/installer.php -O ./install/index.php
+  mysql $DBNAME -e "UPDATE pxp_config SET value = 'on' WHERE  name = 'ffmpeg_sys';" &> /dev/null
+  mysql $DBNAME -e "UPDATE pxp_config SET value = '/usr/bin/ffmpeg' WHERE  name = 'ffmpeg_binary_file';" &> /dev/null
+fi
+
+#wowonder,playtube,deepsound
+if [ "$SCRIPT" = "wowonder-null" ] || [ "$SCRIPT" = "wowonder" ] || [ "$SCRIPT" = "playtube" ] || [ "$SCRIPT" = "deepsound" ]; then
+    scriptsun
+    mysql $DBNAME -e "UPDATE config SET value = 'on' WHERE  name = 'ffmpeg_system';" &> /dev/null
+    mysql $DBNAME -e "UPDATE config SET value = '/usr/bin/ffmpeg' WHERE  name = 'ffmpeg_binary_file';" &> /dev/null
+fi
 
 #quickdate
-     mysql admin_$DB -e "UPDATE options SET option_value = '1' WHERE  option_name = 'ffmpeg_sys';" &> /dev/null
-     mysql admin_$DB -e "UPDATE options SET option_value = '/usr/bin/ffmpeg' WHERE option_name = 'ffmpeg_binary';" &> /dev/null
+if [ "$SCRIPT" = "quickdate" ]; then
+     scriptsun
+     mysql $DBNAME -e "UPDATE options SET option_value = '1' WHERE  option_name = 'ffmpeg_sys';" &> /dev/null
+     mysql $DBNAME -e "UPDATE options SET option_value = '/usr/bin/ffmpeg' WHERE option_name = 'ffmpeg_binary';" &> /dev/null
+fi
 
+# wordpress
+if [ "$SCRIPT" = "wordpress" ]; then
+   wordpress
+fi
+
+chown -R $user:$user $WORKINGDIR
 cat > htaccess_tmp << HERE
 # Redirects http to https protocol
 RewriteCond %{HTTPS} !on
@@ -68,7 +118,7 @@ RewriteRule ^(.*)$ https://%1/\$1 [R=301,L]
 HERE
 sed -i -e '/RewriteEngine/r htaccess_tmp' .htaccess
 
-if grep -wqorP $DOMAIN /home/admin/web/$DOMAIN/public_html; then
+if grep -wqorP $DBNAME /home/admin/web/$DOMAIN/public_html; then
     rm -r ./install  __MACOSX $SCRIPT.zip  &> /dev/null
 
     echo -e "Installation $SCRIPT is successfully:
