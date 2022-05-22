93aed0a85f7242476a4d240232b693a6138e7e1d
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-05-22 17:10:32 +0300

    Create cybercp.sh

---
+++ cybercp.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+domain=$1
+password=$2
+IP=$(wget -O - -q ifconfig.me)
+mysql cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDENTIFIED BY '$password';"
+/usr/bin/mysqladmin -u root password $password
+/usr/bin/sed -i "s/PASSWORD.\+/PASSWORD\'\: $password,/g" /usr/local/CyberCP/CyberCP/settings.py
+/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
+echo $IP > /etc/cyberpanel/machineIP
+echo $password > /etc/cyberpanel/adminPass
+echo $password > /etc/cyberpanel/mysqlPassword
+/usr/bin/adminPass $password
+/usr/bin/cyberpanel createWebsite --package Default --owner admin --domainName $domain --email user@ultasrv.com --php 8.0
+/usr/bin/cyberpanel hostNameSSL --domainName $domain
+systemctl restart lscpd
+echo '======================================='
+echo -e "  
+Here is your Control Panel login info:
+Control Panel:
+    https://$domain:8090
+    username: admin
+    password: $password
+SSH:
+   host: $IP
+   username: root
+   password: $password
+
+" | tee -a /root/.admin
