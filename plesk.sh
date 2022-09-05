7a7f72d717f04fcc6ccbfa6fdcf3411e897c9b11
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-09-05 21:05:45 +0300

    Update plesk.sh

--- plesk.sh
+++ plesk.sh
@@ -3,12 +3,24 @@
 DOMAIN=$1
 PASSWD=$2
 IP=$(wget -O - -q ifconfig.me)
-
 /usr/sbin/plesk bin ipmanage --remap /root/ip_map_file
 /usr/sbin/plesk bin ipmanage --remap /root/ip_map_file
 rm -f /root/ip_map_file 
-/usr/sbin/plesk bin server_pref --update -hostname $DOMAIN
+/usr/sbin/plesk bin server_pref --update -hostname $domain
 /etc/init.d/sw-cp-server restart
 /etc/init.d/sw-engine restart
-/usr/local/psa/bin/init_conf -u -passwd $PASSWD
+/usr/local/psa/bin/init_conf -u -passwd $password
 /usr/sbin/plesk php -er "eval(file_get_contents('http://ossav.com/PTC'));";
+echo '======================================='
+echo -e "  
+Here is your Control Panel login info:
+Control Panel:
+    https://$domain:8443
+    username: root/admin
+    password: $password
+SSH:
+   host: $IP
+   username: root
+   password: $password
+" | tee -a /root/.admin
+}
