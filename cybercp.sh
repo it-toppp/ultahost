ef26cf92c433be00beca1bcd429947fd3a2e57e5
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-09-16 22:14:41 +0300

    Update cybercp.sh

--- cybercp.sh
+++ cybercp.sh
@@ -5,6 +5,7 @@ password=$2
 IP=$(wget -O - -q ifconfig.me)
 #
 /usr/bin/mysqladmin --defaults-file=/root/.my.cnf -u root password $password
+/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
 mysql -uroot -p$password cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDENTIFIED BY '$password';"
 #
 /usr/bin/sed -i "s/'PASSWORD.\+/'PASSWORD'\: '$password',/g" /usr/local/CyberCP/CyberCP/settings.py
@@ -17,17 +18,16 @@ mysql -uroot -p$password cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDEN
 /usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/powerdns/pdns.conf
 /usr/bin/sed -i "s/password =.\+/password = $password/g"/etc/pdns/pdns.conf
 #
-/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
 echo $IP > /etc/cyberpanel/machineIP
 echo $password > /etc/cyberpanel/adminPass
 echo $password > /etc/cyberpanel/mysqlPassword
 /usr/bin/adminPass $password
 /usr/bin/cyberpanel createWebsite --package Default --owner admin --domainName $domain --email user@ultasrv.com --php 8.0
 /usr/bin/cyberpanel hostNameSSL --domainName $domain
-/etc/init.d/ lscpd restart
-/etc/init.d/pure-ftpd-mysql restart
-/etc/init.d/pdns restart
-/etc/init.d/postfix restart
+systemctl restart lscpd
+systemctl restart pure-ftpd-mysql
+systemctl restart pdns
+systemctl restart postfix
 echo '======================================='
 echo -e "  
 Here is your Control Panel login info:
