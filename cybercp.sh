82d4be2a49ff2c6a3adf82b278b7e152b8d47c56
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-09-19 17:50:08 +0300

    Update cybercp.sh

--- cybercp.sh
+++ cybercp.sh
@@ -4,22 +4,21 @@ domain=$1
 password=$2
 IP=$(wget -O - -q ifconfig.me)
 #
-/usr/bin/mysqladmin --defaults-file=/root/.my.cnf -u root password $password
-/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
-mysql -uroot -p$password cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDENTIFIED BY '$password';"
+#/usr/bin/mysqladmin --defaults-file=/root/.my.cnf -u root password $password
+#/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
+#mysql -uroot -p$password cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDENTIFIED BY '$password';"
 #
-/usr/bin/sed -i "s/'PASSWORD.\+/'PASSWORD'\: '$password',/g" /usr/local/CyberCP/CyberCP/settings.py
-
-/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/pureftpd-mysql.conf
-/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/db/mysql.conf
-/usr/bin/sed -i "s/password =.\+/password = $password/g" /etc/postfix/mysql-virtual_*
-/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/powerdns/pdns.conf
-/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/pdns/pdns.conf
-/usr/bin/sed -i "s/password=.\+/password=$password/g" /etc/dovecot/dovecot-sql.conf.ext
+#/usr/bin/sed -i "s/'PASSWORD.\+/'PASSWORD'\: '$password',/g" /usr/local/CyberCP/CyberCP/settings.py
+#/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/pureftpd-mysql.conf
+#/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/db/mysql.conf
+#/usr/bin/sed -i "s/password =.\+/password = $password/g" /etc/postfix/mysql-virtual_*
+#/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/powerdns/pdns.conf
+#/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/pdns/pdns.conf
+#/usr/bin/sed -i "s/password=.\+/password=$password/g" /etc/dovecot/dovecot-sql.conf.ext
 #
 echo $IP > /etc/cyberpanel/machineIP
 echo $password > /etc/cyberpanel/adminPass
-echo $password > /etc/cyberpanel/mysqlPassword
+#echo $password > /etc/cyberpanel/mysqlPassword
 /usr/bin/adminPass $password
 /usr/bin/cyberpanel createWebsite --package Default --owner admin --domainName $domain --email user@ultasrv.com --php 8.0
 /usr/bin/cyberpanel hostNameSSL --domainName $domain
