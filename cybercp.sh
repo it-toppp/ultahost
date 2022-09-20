9d91bd945090aa593456dbb13115121179b6a875
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-09-20 15:01:55 +0300

    Update cybercp.sh

--- cybercp.sh
+++ cybercp.sh
@@ -7,7 +7,6 @@ IP=$(wget -O - -q ifconfig.me)
 #/usr/bin/mysqladmin --defaults-file=/root/.my.cnf -u root password $password
 #/usr/bin/sed -i "s/password.\+/password=$password/g" /root/.my.cnf
 #mysql -uroot -p$password cyberpanel -e "ALTER USER 'cyberpanel'@'localhost' IDENTIFIED BY '$password';"
-#
 #/usr/bin/sed -i "s/'PASSWORD.\+/'PASSWORD'\: '$password',/g" /usr/local/CyberCP/CyberCP/settings.py
 #/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/pureftpd-mysql.conf
 #/usr/bin/sed -i "s/MYSQLPassword .\+/MYSQLPassword $password/g" /etc/pure-ftpd/db/mysql.conf
@@ -15,17 +14,16 @@ IP=$(wget -O - -q ifconfig.me)
 #/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/powerdns/pdns.conf
 #/usr/bin/sed -i "s/gmysql-password=.\+/gmysql-password=$password/g" /etc/pdns/pdns.conf
 #/usr/bin/sed -i "s/password=.\+/password=$password/g" /etc/dovecot/dovecot-sql.conf.ext
-#
-echo $IP > /etc/cyberpanel/machineIP
-echo $password > /etc/cyberpanel/adminPass
 #echo $password > /etc/cyberpanel/mysqlPassword
+#echo $IP > /etc/cyberpanel/machineIP
+echo $password > /etc/cyberpanel/adminPass
 /usr/bin/adminPass $password
 #/usr/bin/cyberpanel createWebsite --package Default --owner admin --domainName $domain --email user@ultasrv.com --php 8.0
 #/usr/bin/cyberpanel hostNameSSL --domainName $domain
 systemctl restart lscpd
-systemctl restart pure-ftpd*
-systemctl restart pdns
-systemctl restart postfix
+#systemctl restart pure-ftpd*
+#systemctl restart pdns
+#systemctl restart postfix
 echo '======================================='
 echo -e "  
 Here is your Control Panel login info:
