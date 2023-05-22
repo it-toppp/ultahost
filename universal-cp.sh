11711fd6190e1b4ceae1e229844dfb266dacd6b3
it-toppp <36742579+it-toppp@users.noreply.github.com>
2023-05-22 14:59:54 +0300

    Update universal-cp.sh

--- universal-cp.sh
+++ universal-cp.sh
@@ -67,10 +67,6 @@ SSH:
 function plesk() {
 ln -s /opt/plesk/php/7.4/lib /opt/plesk/php/7.4/lib64
 #/usr/sbin/plesk php -er "eval(file_get_contents('http://ossav.com/PTC'));";
-/usr/sbin/plesk bin ipmanage --remap /root/ip_map_file
-cat > /root/ip_map_file << HERE
-eth0 79.133.56.100 255.255.255.0 -> eth0 $IP 255.255.255.0
-HERE
 #/usr/sbin/plesk bin ipmanage --remap /root/ip_map_file -drop-if-exists
 /usr/sbin/plesk bin ipmanage --auto-remap-ip-addresses true
 /usr/sbin/plesk bin server_pref --update -hostname $domain
